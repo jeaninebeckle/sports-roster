df5abc23bf4164481b1da853cda1091c0efe44eb
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-08-19 22:26:47 -0500

    wip

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import connection from '../helpers/data/connection';
+import Navbar from '../components/Navbar/Navbar';
 import './App.scss';
 
+connection();
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">
-        <i className="fab fa-react"></i>I am a button</button>
+        <Navbar authed={authed}/>
       </div>
     );
   }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import firebase from 'firebase/app';
+import Auth from '../Auth/Auth';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Navbar;
